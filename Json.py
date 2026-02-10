5a4b0f94d71595023ac9c462881cd7051c20db37
StormenxD <168292547+StormenxD@users.noreply.github.com>
2026-02-10 10:54:40 +0100

    Delete Json.py

--- Json.py
+++
@@ -1,7 +0,0 @@
-{
-  "buildCommand": "pip install -r requirements.txt",
-  "rewrites": [
-    { "source": "/api/(.*)", "destination": "/api/$1" },
-    { "source": "/(.*)", "destination": "/public/$1" }
-  ]
-}
